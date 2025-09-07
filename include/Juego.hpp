7feef1b99e99bdd2ec278c7a4dc8cca9a984c493
Valeria Velazquez <valeriavelazquezvazquez@gmail.com>
2025-09-07 00:23:14 -0600

    Definir la forma en la que se manejaran los estados del programa

--- include/Juego.hpp
+++ include/Juego.hpp
@@ -3,12 +3,19 @@
 #include <SFML/Graphics.hpp>
 #include <InterfazUsuario.hpp>
 
+enum class JuegoState {
+    EXPEDIENTE, //ingreso
+    CIRUGIA,// partida
+    FINAL // final
+};
+
 class Juego {
 private:
     sf::RenderWindow window;
     sf::Font juegoFont;
     InterfazUsuario interfaz;
 
+    JuegoState estadoActual; //guardar el estado en el momento q tiene el juego
 public:
     Juego();
     void run();
