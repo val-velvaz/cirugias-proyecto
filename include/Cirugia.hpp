7f4903561cc0cbb53798fdd087ce949f10e8dc55
Valeria Velazquez <valeriavelazquezvazquez@gmail.com>
2025-09-06 19:00:01 -0600

    Definicion de la clase Cirugia

--- include/Cirugia.hpp
+++ include/Cirugia.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
-class Cirugia {
+#include <string>
+#include <vector>
 
+class Cirugia {
+private:
+    std::string nombrePaciente;
+    std::string nombreCirujano;
+    std::string tipoCirugia;
+
+public:
+    Cirugia();
+
+    void setNombrePaciente(const std::string& nombre);
+    void setNombreCirujano(const std::string& nombre);
+    void setTipoCirujia(const std::string& nombre);
+
+    std::string getNombrePaciente() const;
+    std::string getNombreCirujano() const;
+    std::string getTipoCirujia() const;
 };
