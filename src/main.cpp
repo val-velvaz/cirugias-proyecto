b2775986454a7de4ecdc5969d6555f9d3ca8e8cf
Valeria Velazquez <valeriavelazquezvazquez@gmail.com>
2025-09-06 19:10:35 -0600

    Prueba: Instanciacion de Cirugia

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 // src/main.cpp
 #include <SFML/Graphics.hpp>
+#include <iostream> 
 
 #include "Cirugia.cpp"
 #include "InterfazUsuario.hpp"
 #include "Juego.hpp"
 
 int main() {
+    // probar Cirugia
+    Cirugia m_Cirugia;
+    m_Cirugia.setNombrePaciente("Paciente X");
+    m_Cirugia.setNombreCirujano("Dr. Smith");
+    m_Cirugia.setNombreCirujano("Apendicectomía");
+
+    std::cout << "Paciente: " << m_Cirugia.getNombrePaciente() << std::endl;
+    std::cout << "Cirujano: " << m_Cirugia.getNombreCirujano() << std::endl;
+    std::cout << "Tipo de cirugia: " << m_Cirugia.getTipoCirujia() << std::endl;
+    
     return 0;
 };
