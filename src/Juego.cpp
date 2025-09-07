7a33ce66eaa90391f451963b265ebabe3d8d6c5a
Valeria Velazquez <valeriavelazquezvazquez@gmail.com>
2025-09-06 20:04:22 -0600

    Implementacion de la clase Juego

---
+++ src/Juego.cpp
@@ -0,0 +1,19 @@
+#include "Juego.hpp"
+
+Juego::Juego() {
+    window.create(sf::VideoMode(800, 600), "A Contrarreloj c:");
+}
+
+void Juego::run() {
+    // game loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
+}
