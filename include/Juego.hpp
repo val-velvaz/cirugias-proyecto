4209fbcd774d18b9a6cc9b96a91cccaa4cb607c5
Valeria Velazquez <valeriavelazquezvazquez@gmail.com>
2025-09-06 23:50:08 -0600

    Constructor de Juego  modificado para manejar fuentes

--- include/Juego.hpp
+++ include/Juego.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <InterfazUsuario.hpp>
 
 class Juego {
 private:
     sf::RenderWindow window;
+    sf::Font juegoFont;
+    InterfazUsuario interfaz;
 
 public:
     Juego();
--- src/Juego.cpp
+++ src/Juego.cpp
@@ -1,7 +1,12 @@
-#include "Juego.hpp"
+#include "Juego.hpp" //pendiente: cambiarlo por game
 
 Juego::Juego() {
     window.create(sf::VideoMode(800, 600), "A Contrarreloj c:");
+    // agregar la fuente
+    if (!juegoFont.loadFromFile("arial.ttf")) {
+        window.close();
+    }
+    interfaz.config(juegoFont);
 }
 
 void Juego::run() {
@@ -12,9 +17,11 @@ void Juego::run() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            interfaz.manejarEntrada(event);
         }
         //window.clear();
-        window.clear(sf::Color(173, 216, 230)); // prueba 
+        window.clear(sf::Color(sf::Color::Black)); // prueba 
+        interfaz.draw(window);
         window.display();
     }
 }
